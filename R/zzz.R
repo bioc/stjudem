83f5674a28bc22fc679ceb74205cc4e83d33c3d1
Herve Pages <hpages@fhcrc.org>
2006-09-27 00:29:15 +0000

    Added the stjudem package (Joern Toedling <toedling@ebi.ac.uk> request on Seth's suggestion).
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/stjudem@136 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.First.lib <- function(libname, pkgname){
+  cat("Loading data set 'stjude' ...\n")
+  data(stjude)
+  cat("Type 'summary(stjude)' for an overview of the data structure.\n")
+  cat("See package 'macat' for examplary analyses.\n")
+}
+
+
