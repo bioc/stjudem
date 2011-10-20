38ad2233747c2679b8394746a72543af95c93649
Dan Tenenbaum <dtenenba@fhcrc.org>
2011-10-20 20:12:58 +0000

    For all packages without a namespace and with a .First.lib() function, did the following:
     - Replaced .First.lib() with .onLoad() if function did something
       useful (otherwise removed function).
     - If there is an .onLoad() function, added a generic NAMESPACE file.
     - Bumped version.
    
    Not adding NAMESPACE files to packages without .First.lib()/.onLoad(),
    as the build system will generate those when package versions are bumped
    (when we release BioC 2.9).
    
    
    
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/stjudem@1778 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.First.lib <- function(libname, pkgname){
+.onLoad <- function(libname, pkgname){
   cat("Loading data set 'stjude' ...\n")
   data(stjude)
   cat("Type 'summary(stjude)' for an overview of the data structure.\n")
